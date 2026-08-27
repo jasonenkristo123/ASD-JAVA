e4f794067e20efb8f018cd46466ab4fe9fdbd13a
jasonenkristo123 <jasonenkristo@gmail.com>
2026-08-27 13:48:42 +0700

    arraylist"
    "

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+}
+
+class ArrayListss {
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> malas = new ArrayList<>();
+        malas.add(1);
+        malas.add(2);
+
+        Integer index1 = malas.get(1);
+
+        System.out.println(index1);
     }
 }
